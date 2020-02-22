01d492d4a0966b0e90e04b6f976e9adf1bb0124b
StasyHard <reyngardt.as@yandex.ru>
2020-02-22 11:43:22 +0300

    Create progect

---
+++ Algoritms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algoritms
+//
+//  Created by Anastasia Reyngardt on 22.02.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
