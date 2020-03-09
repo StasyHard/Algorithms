5ab9bcd1cad7d6f57ef2452c4a5de08d55c4838e
StasyHard <reyngardt.as@yandex.ru>
2020-03-09 16:35:45 +0300

    2 task with problem in insert metod

--- Algoritms/main.c
+++ Algoritms/main.c
@@ -7,10 +7,9 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
+//#include <malloc.h>
 #include "main.h"
-#define MaxN 100
+#define MaxN 10
 
 //Рейнгардт Анастасия
 
@@ -41,7 +40,7 @@ int main(int argc, const char * argv[]) {
 }
 
 //MARK: - 1
-/* 1.  */
+/* 1. Реализовать простейшую хеш-функцию. На вход функции подается строка, на выходе сумма кодов символов. */
 
 void solution1() {
     
@@ -50,18 +49,156 @@ void solution1() {
 
 
 //MARK: - 2
-/* 2. */
+/* 2. Переписать программу, реализующую двоичное дерево поиска.
+ а) Добавить в него обход дерева различными способами;
+ б) Реализовать поиск в двоичном дереве поиска;
+ в) *Добавить в программу обработку командной строки, с помощью которой можно указывать, из какого файла считывать данные, каким образом обходить дерево. */
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+} Node;
+
+void printTree(Node* root) {
+    if (root) {
+        printf("%d", root->data);
+        if (root->left || root->right) {
+            printf("(");
+            
+            if (root->left) {
+                printTree(root->left);
+            } else {
+                printf("NULL");
+            }
+            printf(",");
+            
+            if (root->right) {
+                printTree(root->right);
+            } else {
+                printf("NULL");
+            }
+            printf(")");
+        }
+    }
+}
+
+Node* getTreeNode(int value, Node* parent) {
+    Node* tmp = (Node*)malloc(sizeof(Node*));
+    tmp->left = tmp->right = NULL;
+    tmp->data = value;
+    tmp->parent = parent;
+    return tmp;
+}
+
+void insertNode(Node** head, int value) {
+    Node* tmp = NULL;
+    if (*head == NULL) {
+        *head = getTreeNode(value, NULL);
+        return;
+    }
+    tmp = *head;
+    while (tmp) {
+        if (value > tmp->data) {
+            if (tmp->right) {
+                tmp = tmp->right;
+                continue;
+            } else {
+                tmp->right = getTreeNode(value, tmp);
+                return;
+            }
+        }  else if (value < tmp->data) {
+            if (tmp->left) {
+                tmp = tmp->left;
+                continue;
+            } else {
+                tmp->left = getTreeNode(value, tmp);
+                return;
+            }
+        }
+    }
+}
+//Прямой обход бинарного дерева
+void preOrder(Node* root) {
+    if (root == NULL) {
+                return;
+    }
+    if (root->data) {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+//Симметричный обход бинарного дерева
+void inOrder(Node* root) {
+    if (root == NULL) {
+         return;
+    }
+    inOrder(root->left);
+    if (root->data) {
+        printf("%d ", root->data);
+    }
+    inOrder(root->right);
+}
+
+//Обратный обход бинарного дерева
+void postOrder(Node* root) {
+    if (root == NULL) {
+         return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    if (root->data) {
+        printf("%d ", root->data);
+    }
+}
+
+//Поиск в двоичном дереве поиска
+Node *seachInTree(Node* root, int number) {
+    if (root == NULL || root->data == number) {
+        return root;
+    }
+    if (number > root->data) {
+        return seachInTree(root->right, number);
+    } else {
+        return seachInTree(root->left, number);
+    }
+}
 
 void solution2() {
-
+    int array[MaxN] = {10, 7, 5, 6, 16, 9, 15, 20, 21, 4};
+    Node *Tree = NULL;
+    int i;
+    for (i = 0; i < MaxN; i++) {
+        int value = array[i];
+        insertNode(&Tree, value);
+    }
+    printf("Tree");
+    printTree(Tree);
+    printf("\nПрямой обход бинарного дерева - ");
+    preOrder(Tree);
+    printf("\nСимметричный обход бинарного дерева - ");
+    inOrder(Tree);
+    printf("\nОбратный обход бинарного дерева - ");
+    postOrder(Tree);
+    int number;
+    printf("\n Введите одно из чисел - 4, 5, 6, 7, 9, 10, 15, 16, 20, 21: \n");
+    scanf("%d", &number);
+    Node *elem = seachInTree(Tree, number);
+    printf("Parent - %d, data - %d, left element data - %d, right element data - %d", elem->parent->data, elem->data, elem->left->data, elem->right->data);
+    
 }
 
 //MARK: - 3
-/* 3.  */
+/* 3. *Разработать базу данных студентов из полей «Имя», «Возраст», «Табельный номер», в которой использовать все знания, полученные на уроках.
+ Считайте данные в двоичное дерево поиска. Реализуйте поиск по какому-нибудь полю базы данных (возраст, вес). */
+
+
 
 void solution3() {
     
-
+    
 }
 
 
@@ -71,6 +208,5 @@ void menu() {
     printf("1 - task1\n");
     printf("2 - task2\n");
     printf("3 - task3\n");
-    printf("6 - task6\n");
     printf("0 - exit\n");
 }
--- Algoritms/main.h
+++ Algoritms/main.h
@@ -11,27 +11,11 @@
 
 #include <stdio.h>
 
-struct stack;
-void init(struct stack *stk);
-void push(struct stack *stk, int number);
-int pop(struct stack *stk);
-
 void solution1(void);
-void convert_number(struct stack *stk, int number);
 
 void solution2(void);
-void push1(char value);
-char pop1(void);
-void printStack(void);
 
 void solution3(void);
-
-void solution6(void);
-struct queue;
-void initQueue(struct queue *q);
-void insert(struct queue *q, int x);
-void printQueue(struct queue *q);
-int removeQueue(struct queue *q);
 
 void menu(void);
 
